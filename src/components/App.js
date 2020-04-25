402fdad96f6bad64af64e0205c1a609563a152b9
Roger Mayer <rmayer1984@gmail.com>
2020-04-25 18:33:10 -0500

    added notes for reducers

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,14 @@ import React from "react";
 
 const App = () =>{
   return <div>App</div>;
+
 };
 
 export default App;
+
+//REDUCER (states in redux)
+// Song list reducer
+//Selected song reducer
+
+//ACTION CREATOR
+// Select Song
